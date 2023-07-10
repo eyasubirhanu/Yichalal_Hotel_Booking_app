447f4def4f0a0f848e662c20546c7e4dcf7ce0e2
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-07-10 18:09:43 +0300

    added main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
-	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/pkg/config"
-	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/pkg/handlers"
-	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/pkg/render"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/config"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/driver"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/handlers"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/helpers"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/models"
+	"github.com/eyasubirhanu/Yichalal_Hotel_Booking_app/internal/render"
 )
 
-var portNumber = ":8080"
+const portNumber = ":8080"
+
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
+// main is the main application function
 func main() {
+	db, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.SQL.Close()
 
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() (*driver.DB, error) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+	gob.Register(models.User{})
+	gob.Register(models.Room{})
+	gob.Register(models.Restriction{})
+
+	// change this to true when in production
 	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -27,21 +67,27 @@ func main() {
 
 	app.Session = session
 
+	// connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=eyasu password=new_password")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	log.Println("Connected to database!")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		log.Fatal("can't create templet cache")
+		log.Fatal("cannot create template cache")
+		return nil, err
 	}
+
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
+	render.NewRenderer(&app)
+	helpers.NewHelpers(&app)
 
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return db, nil
 }
